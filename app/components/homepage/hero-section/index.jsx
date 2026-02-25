39f7c70d41701929c05c523800d174a5134c4abf
Bhoomika Monthy Rajashekar <bhoomi02@edu.com>
2026-02-24 22:24:38 -0500

    Fix unescaped apostrophe in hero section for Vercel build

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -24,7 +24,7 @@ function HeroSection() {
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-[#58473B] md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hello, <br />
-            I'm {' '}
+            I&apos;m {' '}
             <span className=" text-[#B59E7D]">{personalData.name}</span>
             {` . I'm an  `}
             <span className=" text-[#58473B]">{personalData.designation}</span>
