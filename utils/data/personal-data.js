268db66b2a1d2e5b6ca04d75eb32956626a2b238
Bhoomika Monthy Rajashekar <bhoomi02@edu.com>
2026-09-23 22:14:38 -0400

    Add resume PDF to public folder and update resume link

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,5 +7,5 @@ export const personalData = {
   address: 'Boston, MA',
   github: 'https://github.com/bhoomi02-ai',
   linkedIn: 'https://www.linkedin.com/in/bhoomika-rajshekar/',
-  resume: "https://drive.google.com/drive/folders/1I1t-ZGSqj_Z_khCGKQ-e8jysZhjk1Awu?usp=share_link",
+  resume: "/Bhoomika_nu_resume_ai.pdf",
 }
